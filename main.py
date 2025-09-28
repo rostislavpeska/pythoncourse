8517b259ad5b2c99b3c8ed7eab997bb332353050
rostislavpeska <40421596+rostislavpeska@users.noreply.github.com>
2025-09-28 13:54:48 +0200

    Functions

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-x = 5
+"""def my_power(n1: int, n2: int) -> int:
+    return n1 ** n2
+
+res = my_power(10,3)
+print(res)
+
+def get_numbers() -> tuple[int, int]:
+    return 1, 3
+
+# print(get_numbers())
+
+def work() -> None:
+    x, y = get_numbers()
+    print(get_numbers())
+
+work()
+res = get_numbers()
+print(res)
+"""
+x = 1
+
+def greet_user(name: str = "user") -> None:
+    print("Hello, welcome to the syestem,", name)
+
+greet_user()
+greet_user("admin")
