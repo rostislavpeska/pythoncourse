06d25eccca50e1d6856636b325f6d23cf52c96df
rostislavpeska <40421596+rostislavpeska@users.noreply.github.com>
2025-10-01 19:33:14 +0200

    Added Inheritance

--- animal.py
+++ animal.py
@@ -1,9 +1,12 @@
-class Animal:
+from abc import abstractmethod, ABC
+
+class Animal (ABC):
     def __init__(self, chip_num: int,  age: int, says: str = "...") -> None:
         self._chip_num = chip_num
         self.age = age
         self.says = says
 
+    @abstractmethod
     def _make_sound(self) -> None:
         print(f"This is an animal. It is {self.age} and says {self.says}.")
 
